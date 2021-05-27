a92d7d0e3162452d83ffc46ae252f1f35e249488
Alejandro Melo <dmelo@unal.edu.co>
2021-05-27 00:05:02 -0500

    Improving multi client functionality
    Changes:
    - Socket creating architecture checked
    - Server shutdown from client added
    - Searching functions verified

--- lib/app/utils/network_connection.c
+++ lib/app/utils/network_connection.c
@@ -1,33 +1,85 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
 #include <strings.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "search_data.c"
 #define PORT 3535
 #define BACKLOG 8
 
 // Program constants
 int SOCKET_DOMAIN = AF_INET, SOCKET_TYPE = SOCK_STREAM , SOCKET_PROTOCOL = 0;
+bool flags [32] = {false};
+HashTable *table;
 
 /**
  * the function sendError is used to interrupt the program when something goes wrong
  * @param message to be printed in the error explanation
 */
-void sendError(char message[]){
+void sendError(const char message[]){
     perror(message);
     exit(EXIT_FAILURE);
+}
+
+void check(int exp, const char message[]){
+    if (exp < 0) sendError(message);
+}
+
+void * handleConnection(void * data){
+    int clientfd;
+    char buffer[20];
+
+    clientfd = *((int *) data);
+
+    printf("hilo iniciado con el socket %d\n", clientfd);
+
+    processQuery(table, clientfd);
+}
+
+void acceptClients(int serverfd){
+    struct sockaddr_in clientAddress;
+    int clientfd, client = 0;
+    pthread_t pid;
+    socklen_t clientSize = sizeof(clientAddress);
+
+    // Recive all client connections
+    printf("Esperando conexiones ...\n");
+    while (true){
+        // Check if thread ended client communication
+        pthread_join(pid, NULL);
+
+        // Accept new client connection
+        clientfd = accept(serverfd, (struct sockaddr *) &clientAddress, &clientSize);
+        check (clientfd, "error en conexión con cliente");
+
+        printf("se ha aceptado una conexión correctamente\n");
+
+        // create new thread for attend new client
+        check(pid = pthread_create(&(pid), NULL, (void *) handleConnection, (void *) &clientfd), 
+            "ha ocurrido un error al crear el hilo para la conexión");
+        printf("se ha creado correctamente un hilo para el cliente \n");
+
+        if (client == 31) client = 0;
+        else client++;
+    }
 }
 
 /**
  * the function serverConnection is used to generate a new active listening port for server
  * @return the id of the server socket to read queries
 */
-int serverConnection(){
+void serverConnection(){
     struct sockaddr_in address;
     int opt = 1;
+
+    // Loading hashtable for searching process
+    table = initHash();
+    loadHash(table);
 
     // Creating socket file descriptor
     int serverfd = socket(SOCKET_DOMAIN, SOCKET_TYPE, SOCKET_PROTOCOL);
@@ -39,17 +91,18 @@ int serverConnection(){
     bzero(address.sin_zero,8);
 
     // Setting socket options
-    if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0)
-        sendError("error en opciones del socket");
+    check(setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)),
+        "error en opciones del socket");
     
     // Binding socket
-    if (bind(serverfd, (struct sockaddr*) &address, sizeof(struct sockaddr)) < 0)
-        sendError("error en bind");
+    check(bind(serverfd, (struct sockaddr*) &address, sizeof(struct sockaddr)),
+        "error en bind");
 
     // Activating listening mode in socket
-    if (listen(serverfd, BACKLOG) < 0) sendError("error en listen");
+    check(listen(serverfd, BACKLOG), "error en listen");
 
-    return serverfd;
+    // accept all 32 clients
+    acceptClients(serverfd);
 }
 
 /**
@@ -76,3 +129,4 @@ int clientConnection(){
 
     return clientfd;
 }
+ 
